d1b1395a52997452427b88e18c921683fdb0291f
duongvn <ddvnguyen92@gmail.com>
2017-08-20 20:30:45 -0600

    updated solgan generator

--- phaseOmatic.js
+++ phaseOmatic.js
@@ -1,6 +1,15 @@
 function phaseOmatic(){ // initializing function for later calling
+	var words1=["24/7", "MULTI-TIER", "30,000 FEET", "B2B", "WIN-WIN"];
+	var moreWords=["EMPOWERED", "VALUE-ADDED", "ORIENTED", "FOCUSED", "ASYMMETRICAL"];
+	var lastWord=["PROCESS", "SOLUTION", "TIPPING-POINT", "STRATEGY", "VISION"];
+	//generating random numbers between 0 and the last index array.
+	var random1 = Math.floor(Math.random()*words1.length);
+	var random2 = Math.floor(Math.random()*moreWords.length);
+	var random3 = Math.floor(Math.random()*lastWord.length);
 	
-	
-	
+	//concatenating all generated random phases into marketing slogan
+	var phrase = words1[random1]+" "+moreWords[random2]+" "+lastWord[random3];
+	//displaying output
+	alert(phrase);
 }
 phaseOmatic(); //calling
